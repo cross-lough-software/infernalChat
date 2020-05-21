c3bac52ec30581c6b078c9c3077b69000fc1f80a
thran <awwfeck+dev@pm.me>
2020-05-21 15:09:45 +0100

    Initial commit of helper script and main script.

---
+++ infChat/infChat.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# CROSS LOUGH SOFTWARE
+# infChat main script
+# Authors: lordfeck
+# Authored: 21/05/2020
+
+use warnings;
+use strict;
+
+print "As of today, I have nothing to say.\n";
